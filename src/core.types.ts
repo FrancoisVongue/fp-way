751358014e5ce13653419c73dd0e18e1c1cb0862
Francois Vongue <francoisvongue@gmail.com>
2021-03-20 19:05:41 +0200

    added obj deep copy

--- src/core.types.ts
+++ src/core.types.ts
@@ -9,37 +9,37 @@ export type Predicate<T1=any, T2=any, T3=any, T4=any> = {
 export type BinaryPredicate<T1, T2> = (arg1: T1, arg2: T2) => boolean
 export type Unary<T1, R> =
     (singleArg: T1) => R
-export type Binary<T1, T2, R> = 
-    (firstArg: T1, secArg: T2) => R 
-export type Ternary<T1, T2, T3, R> = 
-    (firstArg: T1, secArg: T2, thirdArg: T3) => R 
-export type Quaternary<T1, T2, T3, T4, R> = 
-    (firstArg: T1, secArg: T2, thirdArg: T3, forthArg: T4) => R 
+export type Binary<T1, T2, R> =
+    (firstArg: T1, secArg: T2) => R
+export type Ternary<T1, T2, T3, R> =
+    (firstArg: T1, secArg: T2, thirdArg: T3) => R
+export type Quaternary<T1, T2, T3, T4, R> =
+    (firstArg: T1, secArg: T2, thirdArg: T3, forthArg: T4) => R
 export type Entry<T1, T2> = [T1, T2];
-export type DTO = {[key: string]: any};
+export type DTO = Record<string, any>;
 export type Entries<T,K> = Entry<T,K>[];
 
 export interface Curried1<T1, R> {
-    (): Curried1<T1, R>;	
-    (t1: T1): R;	
+    (): Curried1<T1, R>;
+    (t1: T1): R;
 }
 export interface Curried2<T1, T2, R> {
-    (): Curried2<T1, T2, R>;	
-    (t1: T1): Curried1<T2, R>;	
-    (t1: T1, t2: T2): R;	
-}	
-export interface Curried3<T1, T2, T3, R> {	
-    (): Curried3<T1, T2, T3, R>;	
-    (t1: T1): Curried2<T2, T3, R>;	
-    (t1: T1, t2: T2): Curried1<T3, R>;	
-    (t1: T1, t2: T2, t3: T3): R;	
-}	
-export interface Curried4<T1, T2, T3, T4, R> {	
-    (): Curried4<T1, T2, T3, T4, R>;	
-    (t1: T1): Curried3<T2, T3, T4, R>;	
-    (t1: T1, t2: T2): Curried2<T3, T4, R>;	
-    (t1: T1, t2: T2, t3: T3): Curried1<T4, R>;	
-    (t1: T1, t2: T2, t3: T3, t4: T4): R;	
+    (): Curried2<T1, T2, R>;
+    (t1: T1): Curried1<T2, R>;
+    (t1: T1, t2: T2): R;
+}
+export interface Curried3<T1, T2, T3, R> {
+    (): Curried3<T1, T2, T3, R>;
+    (t1: T1): Curried2<T2, T3, R>;
+    (t1: T1, t2: T2): Curried1<T3, R>;
+    (t1: T1, t2: T2, t3: T3): R;
+}
+export interface Curried4<T1, T2, T3, T4, R> {
+    (): Curried4<T1, T2, T3, T4, R>;
+    (t1: T1): Curried3<T2, T3, T4, R>;
+    (t1: T1, t2: T2): Curried2<T3, T4, R>;
+    (t1: T1, t2: T2, t3: T3): Curried1<T4, R>;
+    (t1: T1, t2: T2, t3: T3, t4: T4): R;
 }
 
 export type TCurry = {
