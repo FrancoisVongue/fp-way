c12862e5f08582176d16f3d056bcaa6edb290e0b
Francois <francoisvongue@gmail.com>
2022-01-29 13:46:34 +0200

    added string

--- src/str/index.test.ts
+++ src/str/index.test.ts
@@ -1,5 +1,37 @@
-describe('test', () => {
-    it('should run', () => {
-        expect(0).toBe(0);
+import {str} from "./index";
+
+describe('ConcatWith', () => {
+    it('should concat two strings', () => {
+        const hello = 'hello';
+        const world = ' world';
+        const plusWorld = str.ConcatWith(world);
+        const hw = plusWorld(hello);
+        
+        expect(hw).toBe(hello + world);
+    })
+})
+
+describe('Matches', () => {
+    it('should check whether string matches regex', () => {
+        const email = 'hello@mail.com'
+        const notEmail = 'random string';
+        const emailRegex = /.*@\w{1,6}\.\w{2,5}$/;
+        const matchesEmail = str.Matches(emailRegex);
+        
+        const True = matchesEmail(email);
+        const False = matchesEmail(notEmail);
+
+        expect(True).toBe(true);
+        expect(False).toBe(false);
+    })
+})
+
+describe('OccurrencesOf', () => {
+    it('should return every occurrence of the pattern', () => {
+        const threeMoo = `"Moooo". Who said that? It was a cow, they moo all the time. "Moooooo"`;
+        const mooRegex = /[Mm]o+/;
+        const Moos = str.OccurrencesOf(mooRegex, threeMoo);
+        
+        expect(Moos.length).toBe(3);
     })
 })
