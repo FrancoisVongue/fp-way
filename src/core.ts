9d6695e5fd0b0b88528374fe5eccbb4949b61e8c
Francois <francoisvongue@gmail.com>
2022-02-17 17:56:10 +0200

    latest

--- src/core.ts
+++ src/core.ts
@@ -1,10 +1,10 @@
 import {
-  Binary, Curried,
-  Curried2, Curried3,
-  JSTypesWithArrayAndNull,
-  Predicate,
-  TCurry,
-  Unary, UnaryPredicate
+    Binary, Curried,
+    Curried2, Curried3,
+    JSTypesWithArrayAndNull,
+    Predicate,
+    TCurry,
+    Unary, UnaryPredicate
 } from "./core.types";
 
 export const Curry: TCurry = function (f, ...initialArgs) {
@@ -36,63 +36,63 @@ export const Call = Curry((f, v) => f(v))
 export const ApplyOn = Swap(Call)
 
 export const IfElse: {
-  <T, R>(
-    predicate: Unary<T, boolean>,
-    onSuccess: Unary<T, R>,
-    onFail: Unary<T, R>,
-    value: T
-  ): R
-  
-  <T, R>(
-    predicate: Unary<T, boolean>,
-    onSuccess: Unary<T, R>,
-    onFail: Unary<T, R>,
-  ): Unary<T, R>
-  
-  <T, R>(
-    predicate: Unary<T, boolean>,
-    onSuccess: Unary<T, R>,
-  ): Curried2<Unary<T, R>, T, R>
-  
-  <T, R>(
-    predicate: Unary<T, boolean>,
-  ): Curried3<Unary<T, R>, Unary<T, R>, T, R>
+    <T, R>(
+        predicate: Unary<T, boolean>,
+        onSuccess: Unary<T, R>,
+        onFail: Unary<T, R>,
+        value: T
+    ): R
+
+    <T, R>(
+        predicate: Unary<T, boolean>,
+        onSuccess: Unary<T, R>,
+        onFail: Unary<T, R>,
+    ): Unary<T, R>
+
+    <T, R>(
+        predicate: Unary<T, boolean>,
+        onSuccess: Unary<T, R>,
+    ): Curried2<Unary<T, R>, T, R>
+
+    <T, R>(
+        predicate: Unary<T, boolean>,
+    ): Curried3<Unary<T, R>, Unary<T, R>, T, R>
 } = Curry((
     p, onSuccess, onFail, arg
 ) => p(arg) ? onSuccess(arg) : onFail(arg));
 
 export const When: {
-  <T>(
-    predicate: Unary<T, boolean>,
-    onSuccess: Unary<T, T>,
-    value: T
-  ): T
-  
-  <T>(
-    predicate: Unary<T, boolean>,
-    onSuccess: Unary<T, T>,
-  ): Unary<T, T>
-  
-  <T>(
-    predicate: Unary<T, boolean>,
-  ): Curried2<Unary<T, T>, T, T>
+    <T>(
+        predicate: Unary<T, boolean>,
+        onSuccess: Unary<T, T>,
+        value: T
+    ): T
+
+    <T>(
+        predicate: Unary<T, boolean>,
+        onSuccess: Unary<T, T>,
+    ): Unary<T, T>
+
+    <T>(
+        predicate: Unary<T, boolean>,
+    ): Curried2<Unary<T, T>, T, T>
 } = Curry((p, f, a) => p(a) ? f(a) : a);
 
 export const Unless: {
-  <T>(
-    predicate: Unary<T, boolean>,
-    onFail: Unary<T, T>,
-    value: T
-  ): T
+    <T>(
+        predicate: Unary<T, boolean>,
+        onFail: Unary<T, T>,
+        value: T
+    ): T
 
-  <T>(
-    predicate: Unary<T, boolean>,
-    onFail: Unary<T, T>,
-  ): Unary<T, T>
+    <T>(
+        predicate: Unary<T, boolean>,
+        onFail: Unary<T, T>,
+    ): Unary<T, T>
 
-  <T>(
-    predicate: Unary<T, boolean>,
-  ): Curried2<Unary<T, T>, T, T>
+    <T>(
+        predicate: Unary<T, boolean>,
+    ): Curried2<Unary<T, T>, T, T>
 } = Curry((p, f, a) => !p(a) ? f(a) : a);
 
 export const InCase: {
@@ -117,16 +117,16 @@ export const InCase: {
 });
 
 export const IndependentInCase: {
-  <T, R>(
-    entries: [Unary<T, boolean>, Unary<T, R>][],
-  ): (v: T) => R
+    <T, R>(
+        entries: [Unary<T, boolean>, Unary<T, R>][],
+    ): (v: T) => R
 
-  <T, R>(
-    entries: [Unary<T, boolean>, Unary<T, R>][],
-    v: T
-  ): R[]
+    <T, R>(
+        entries: [Unary<T, boolean>, Unary<T, R>][],
+        v: T
+    ): R[]
 } = Curry((
-  entries: [Unary<any, boolean>, Unary<any, any>][],
+    entries: [Unary<any, boolean>, Unary<any, any>][],
     v: any
 ): any[] => {
     const results = [];
@@ -142,27 +142,27 @@ export const IndependentInCase: {
 });
 
 export const CanBeDescribedAs: {
-  <T>(
-    predicates: UnaryPredicate<T>[],
-    value: T
-  ): boolean
-  
-  <T>(
-    predicates: UnaryPredicate<T>[],
-  ): Unary<T, boolean>
+    <T>(
+        predicates: UnaryPredicate<T>[],
+        value: T
+    ): boolean
+
+    <T>(
+        predicates: UnaryPredicate<T>[],
+    ): Unary<T, boolean>
 } = Curry((ps: Predicate[], v: any) => {
     return ps.map(ApplyOn(v)).every(Is(true))
 })
 
 export const Pipe: {
-  <T, R>(
-    functions: [Unary<T, any>, ...Unary<any, any>[], Unary<any, R>],
-    v: T
-  ): R
-  
-  <T, R>(
-    functions: [Unary<T, any>, ...Unary<any, any>[], Unary<any, R>],
-  ): Unary<T, R>
+    <T, R>(
+        functions: [Unary<T, any>, ...Unary<any, any>[], Unary<any, R>],
+        v: T
+    ): R
+
+    <T, R>(
+        functions: [Unary<T, any>, ...Unary<any, any>[], Unary<any, R>],
+    ): Unary<T, R>
 } = Curry((fns: Function[], arg: any): any => {
     let currentArg = arg;
 
@@ -173,14 +173,14 @@ export const Pipe: {
 });
 
 export const Compose: {
-  <T, R>(
-    functions: [Unary<any, R>, ...Unary<any, any>[], Unary<T, any>],
-    v: T
-  ): R
+    <T, R>(
+        functions: [Unary<any, R>, ...Unary<any, any>[], Unary<T, any>],
+        v: T
+    ): R
 
-  <T, R>(
-    functions: [Unary<any, R>, ...Unary<any, any>[], Unary<T, any>],
-  ): Unary<T, R>
+    <T, R>(
+        functions: [Unary<any, R>, ...Unary<any, any>[], Unary<T, any>],
+    ): Unary<T, R>
 } = Curry((fns: Function[], a: any) => Pipe(fns.reverse() as any, a));
 
 export const IsOfType = Curry((desiredType: JSTypesWithArrayAndNull, v: any) => {
@@ -188,9 +188,9 @@ export const IsOfType = Curry((desiredType: JSTypesWithArrayAndNull, v: any) => 
 });
 
 export const TypeOf = (v): JSTypesWithArrayAndNull => {
-  return InCase([
-    [Is(null), () => "null"],
-    [a => Array.isArray(a), () => "array"],
-    [TRUE, a => typeof a]
-  ], v);
+    return InCase([
+        [Is(null), () => "null"],
+        [a => Array.isArray(a), () => "array"],
+        [TRUE, a => typeof a]
+    ], v);
 }
--- src/obj/index.ts
+++ src/obj/index.ts
@@ -4,19 +4,19 @@ import { DataObject, Unary, DeepPartial } from "../core.types";
 export namespace obj {
     export const Keys = <T1 extends DataObject>(obj: T1): (keyof T1 & string)[] => Object.keys(obj);
     export const Entries = <T1 extends DataObject>(obj: T1): [(keyof T1 & string), any][] => Object.entries(obj);
-    export const FromEntries = <T1>(entries: [string, any][]) => 
-      entries.reduce((b, [k, v]) => {
-        b[k] = v
-        return b
-      }, {})
-    
+    export const FromEntries = <T1>(entries: [string, any][]) =>
+        entries.reduce((b, [k, v]) => {
+            b[k] = v
+            return b
+        }, {})
+
     export const DeepCopy = <T1>(obj: T1): T1 => {
         return InCase<T1, T1>([
             [IsOfType('array'), arr => (arr as any).map(DeepCopy) as any],
             [IsOfType('object'), obj => {
                 const newObj = {} as T1;
                 const keys = Keys(obj as any)
-                
+
                 keys.forEach((prop) => {
                     const value = obj[prop];
                     newObj[prop] = DeepCopy(value);
@@ -41,7 +41,7 @@ export namespace obj {
         const objCopy = DeepCopy(obj);
         const defCopy = DeepCopy(def);
         const allProps = [...new Set([...Keys(objCopy), ...Keys(defCopy)])];
-        
+
         for(const key of allProps) {
             objCopy[key] = InCase([
                 [([defv, v]) => [defv, v].every(IsOfType("object")), ([n, o]) => WithDefault(n, o)], // if both are objects, merge them again
@@ -49,7 +49,7 @@ export namespace obj {
                 [TRUE, ([defv, _]) => defv],          // if there's no value, return default
             ], [defCopy[key], objCopy[key]])
         }
-        
+
         return objCopy;
     });
 
@@ -63,7 +63,7 @@ export namespace obj {
             def: DeepPartial<T1>,
         ): R
     } = Swap(WithDefault);
-    
+
     export const Pick: {
         <T1 extends DataObject>(
             keys: (keyof T1)[],
@@ -73,14 +73,14 @@ export namespace obj {
         <T1 extends DataObject>(
             keys: (keyof T1)[],
         ): Unary<T1, Partial<T1>>
-    } = Curry((props, obj) => { 
+    } = Curry((props, obj) => {
         const newObj = {};
         const objCopy = DeepCopy(obj);
         props.forEach(p => newObj[p] = objCopy[p]);
 
         return newObj;
     });
-    
+
     export const ExcludeProps: {
         <T1 extends DataObject>(
             keys: (keyof T1)[],
@@ -123,14 +123,14 @@ export namespace obj {
         for(let row of mapSpec.map) {
             const [sourceProp, mapper, destinationProp] = row;
 
-            result[destinationProp] = sourceProp 
+            result[destinationProp] = sourceProp
                 ? mapper(src[sourceProp], src)
                 : mapper(null, src);
         }
 
         return result;
     });
-    
+
     // == VALIDATOR
     export type ValidationSummary<T1> = {
         valid: boolean,
@@ -141,8 +141,8 @@ export namespace obj {
     }
     namespace _ValidationSummary {
         export const incErrCount = (s: ValidationSummary<any>) => {
-          s.errorCount++
-          s.valid = false
+            s.errorCount++
+            s.valid = false
         }
         export const addErr = (k, msg, summary: ValidationSummary<any>) => {
             if(IsOfType('array', summary.errors[k])) {
@@ -162,21 +162,22 @@ export namespace obj {
             }
         }
         export const mergeNestedSummary = (
-          summary: ValidationSummary<any>,
-          key: string,
-          nestedSummary: ValidationSummary<any>,
+            summary: ValidationSummary<any>,
+            key: string,
+            nestedSummary: ValidationSummary<any>,
         ) => {
-          summary.valid = nestedSummary.valid && summary.valid
-          summary.errorCount += nestedSummary.errorCount
-          summary.missingProperties = [...summary.missingProperties, ...nestedSummary.missingProperties]
-          summary.redundantProperties = [...summary.redundantProperties, ...nestedSummary.redundantProperties]
-          const nestedErrors: [any, any][] = Entries(nestedSummary.errors)
-            .map(([nestedKey, v]) => [`${key}.${nestedKey}`, v])
-          
-          summary.errors = FromEntries([
-            ...Entries(summary.errors),
-            ...nestedErrors
-          ])
+            summary.valid = nestedSummary.valid && summary.valid
+            summary.errorCount += nestedSummary.errorCount
+            summary.missingProperties = [...summary.missingProperties, ...nestedSummary.missingProperties]
+            summary.redundantProperties = [...summary.redundantProperties, ...nestedSummary.redundantProperties]
+            
+            const nestedErrors: [any, any][] = Entries(nestedSummary.errors)
+                .map(([nestedKey, v]) => [`${key}.${nestedKey}`, v])
+
+            summary.errors = FromEntries([
+                ...Entries(summary.errors),
+                ...nestedErrors
+            ])
         }
     }
     export type ValidationException = {
@@ -199,11 +200,11 @@ export namespace obj {
         errorHandler: ({key}) => `Could not validate property: ${key}`,
     }
     export type ValidationPropertyRule<T1> = [
-        (v: any, o: T1) => boolean, 
-        string | ((v: any, k: keyof T1) => string)
+        (v: any, o: T1) => boolean,
+            string | ((v: any, k: keyof T1) => string)
     ];
     export const ValidationOptionsSym: unique symbol = Symbol.for('fp-way-validation-options');
-    export type ValidationSpec<T1 extends DataObject> = 
+    export type ValidationSpec<T1 extends DataObject> =
         & Record<keyof T1, ValidationPropertyRule<T1>[] | any>
         & { [ValidationOptionsSym]: ValidationOptions<T1> };
     export type _CheckPropsResult = {
@@ -212,14 +213,14 @@ export namespace obj {
         propsToCheck: string[],
     }
     export const _validationPreCheckProps = <T1 extends DataObject>(
-        spec: ValidationSpec<T1>, 
+        spec: ValidationSpec<T1>,
         o: T1
     ): _CheckPropsResult => {
         const declaredPropsToCheck = Keys(spec);
         const desiredOptionalProps = spec[ValidationOptionsSym].optionalProps;
         const optionalProps: string[] = When(
-            Is('*'), 
-            Return(declaredPropsToCheck), 
+            Is('*'),
+            Return(declaredPropsToCheck),
             desiredOptionalProps
         );
         const requiredProps = declaredPropsToCheck.filter(d => !optionalProps.includes(d));
@@ -233,11 +234,11 @@ export namespace obj {
         }
 
         const presentProps = Entries(o)
-            .filter(([k, v]) => Exists(v))
-            .map(([k, v]) => k);
-        
+        .filter(([k, v]) => Exists(v))
+        .map(([k, v]) => k);
+
         const missingRequiredProps = requiredProps.filter(r => !presentProps.includes(r));
-        const redundantProps = presentProps.filter(p => !declaredPropsToCheck.includes(p)); 
+        const redundantProps = presentProps.filter(p => !declaredPropsToCheck.includes(p));
         const propsToCheck = presentProps.filter(p => declaredPropsToCheck.includes(p));
 
         return {
@@ -246,19 +247,19 @@ export namespace obj {
             redundant: redundantProps,
         }
     }
-    
+
     export const Validate = <T1 extends DataObject>(
         spec: ValidationSpec<T1>,
         obj: T1
     ): ValidationSummary<T1> => {
         const options = WithDefault<
-          ValidationOptions<any>, 
-          PopulatedValidationOptions<any>
-        >(
-          defaultValidationOptions, 
-          spec[ValidationOptionsSym]
+            ValidationOptions<any>,
+            PopulatedValidationOptions<any>
+            >(
+            defaultValidationOptions,
+            spec[ValidationOptionsSym]
         );
-        
+
         const summary: ValidationSummary<T1> = _ValidationSummary.New();
         const {propsToCheck, missing, redundant} = _validationPreCheckProps(spec, obj);
         if(missing.length) {
@@ -266,42 +267,42 @@ export namespace obj {
             summary.missingProperties = missing;
         }
         if(redundant.length) {
-          summary.redundantProperties = redundant;
-          if(options.redundantIsError) {
-            _ValidationSummary.incErrCount(summary);
-          }
+            summary.redundantProperties = redundant;
+            if(options.redundantIsError) {
+                _ValidationSummary.incErrCount(summary);
+            }
         }
-        
+
         for(const [i, ptc] of propsToCheck.entries()) {
             if(options.stopWhen(summary)) { return summary; }
-            
+
             const keySpec: ValidationPropertyRule<T1>[] | ValidationSpec<any> = spec[ptc];
             const value = obj[ptc];
 
-          if(IsOfType('array', keySpec)) {
+            if(IsOfType('array', keySpec)) {
                 for(const rule of keySpec as ValidationPropertyRule<T1>[]) {
                     const [validator, msgOrFn] = rule;
-                    let rulePass; 
+                    let rulePass;
                     try {
-                      rulePass = validator(value, obj);
+                        rulePass = validator(value, obj);
                     } catch(e){
-                      const message = options.errorHandler({key: ptc, value, ruleIndex: i, error: e})
-                      _ValidationSummary.addErr(ptc, message, summary)
-                      continue;
+                        const message = options.errorHandler({key: ptc, value, ruleIndex: i, error: e})
+                        _ValidationSummary.addErr(ptc, message, summary)
+                        continue;
                     }
 
                     if(!rulePass) {
-                      const message: string = InCase([
-                        [IsOfType('string'), ReturnAsIs()],
-                        [TRUE, f => f(value, ptc)],
-                      ], msgOrFn)
+                        const message: string = InCase([
+                            [IsOfType('string'), ReturnAsIs()],
+                            [TRUE, f => f(value, ptc)],
+                        ], msgOrFn)
 
-                      _ValidationSummary.addErr(ptc, message, summary);
+                        _ValidationSummary.addErr(ptc, message, summary);
                     }
                 }
             } else {
                 const nestedSummary = Validate(keySpec as ValidationSpec<T1>, value)
-                _ValidationSummary.mergeNestedSummary(summary, ptc, nestedSummary)  
+                _ValidationSummary.mergeNestedSummary(summary, ptc, nestedSummary)
             }
         }
     }
